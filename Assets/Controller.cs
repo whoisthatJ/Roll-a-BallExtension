d263ff9a6044a9141910cad71ac10bcb62ae8922
whoisthatJ <Marat.J.Urmanov@gmail.com>
2024-10-14 14:54:09 +0500

    Added environment and player as prefabs

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -9,7 +9,7 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();        
     }
 
     // Update is called once per frame
